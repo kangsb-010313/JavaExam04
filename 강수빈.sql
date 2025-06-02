550bf6c0e19cfde68c4698d493a5eccc933e1702
kangsb-010313 <kangsb668@naver.com>
2025-06-02 10:16:48 +0900

    jExam04 sql 250602 10:16

--- 강수빈.sql
+++ 강수빈.sql
@@ -9,16 +9,32 @@
 월급이 많은 직원부터 출력하세요
 컬럼명은 괄호의 영어로 출력되도록 하세요
 */
+select  employee_id
+        ,first_name
+        ,salary
+        ,department_id 
+from employees
+where department_id = 100
+;
 
 
 
 
 /* 문제2
-월급이 12000 초과인 직원의 이름(first_name), 입사일(hire_date), 월급(salary), 사번(employee_id)을 입사일이 빠른직원부터 출력하세요
+월급이 12000 초과인 직원의 이름(first_name), 입사일(hire_date), 월급(salary), 사번(employee_id)을 
+입사일이 빠른직원부터 출력하세요
 월급은 24,000 원 과 같이 출력하세요( 3자리 마다 콤마  그리고 소수첫째자리에서 반올림)
 입사일은 2003/06/07 형식으로 출력하세요 
 컬럼명은 괄호의 영어로 출력되도록 하세요
 */
+select  first_name
+        ,date_format(hire_date, '%Y/%m/%d') as 'hire_date'
+        ,format(salary, 0) as salary
+        ,employee_id
+from employees
+where salary > 12000
+order by hire_date asc
+;
 
 
 
@@ -29,6 +45,12 @@
 출력은 최고월급(maxSalary), 평균월급(avgSalary), 부서번호(department_id), 
 컬럼명은 괄호의 영어로 출력되도록 하세요
 */
+select  max(salary) maxSalary
+		,avg(salary) avgSalary
+	    ,department_id 'department_id'
+from employees
+group by department_id
+;
 
 
 
@@ -39,6 +61,17 @@
 출력 형식은 이름(first_name), 월급(salary), 부서번호(department_id), 부서명(department_name)
 부서가 없은 직원도 출력하며 부서번호가 낮은부서 부터 출력하세요.
 */
+select  e.first_name
+        ,e.salary
+        ,e.department_id
+        ,d.department_name
+from employees e
+left outer join departments d
+   on e.department_id = d.department_id
+where e.salary >= 7000
+and e.salary < 8000
+order by department_id asc
+;
 
 
 
@@ -49,6 +82,34 @@
 출력 형식은 사번, 이름, 월급, 부서번호 이며
 월급이 높은직원 부터 출력하세요
 */
+-- 부서번호가 60인 부서의 최고월급 
+select  department_id
+        ,max(salary) -- 9000
+from employees
+where department_id = 60
+;
+-- 
+select  employee_id 사번
+        ,first_name 이름
+        ,salary 월급
+        ,department_id 부서번호 
+from employees
+where salary > 9000
+order by salary desc
+;
+-- 합치기 
+select  employee_id 사번
+        ,first_name 이름
+        ,salary 월급
+        ,department_id 부서번호 
+from employees
+where salary > (select  max(salary) -- 9000
+				from employees
+				where department_id = 60)
+order by salary desc
+;
+
+
 
 
 
